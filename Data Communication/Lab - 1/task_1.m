d06390e83476e85c34ba85f762ee0a7c3fffac4d
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2024-02-05 18:25:06 +0600

    commit

---
+++ Data Communication/Lab - 1/task_1.m
@@ -0,0 +1,35 @@
+clc
+close all
+
+%22-46026-1
+%AB-CDEFG-H
+
+%K1 = A + B
+k1 = 4;
+%K2 = G+H+2
+k2 = 9;
+% J1 = D+G+20
+j1 = deg2rad(32);
+% j2 = 30 degrees
+j2 = deg2rad(30);
+
+%f1 = E+F+5
+f1 = 7;
+% f2 = C+D+5
+f2 = 15;
+
+T = 0.1428;
+t = 0: (T/100) : 3*T;
+
+x1 = k1 * cos(2 * pi * f1 * t + j1);
+x2 = k2 * cos(2 * pi * f2 * t + j2);
+
+plot(t, x1)
+title('plotting AC voltage over time')
+xlabel('time')
+ylabel('voltage')
+figure()
+plot(t, x2)
+title('plotting AC voltage over time')
+xlabel('time')
+ylabel('voltage')
